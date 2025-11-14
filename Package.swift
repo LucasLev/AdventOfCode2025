2378e0c8aacf683216fd567377541547b6fccb63
LucasLev <60597983+LucasLev@users.noreply.github.com>
2025-11-14 17:27:01 -0500

    Switched to apple swift format

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "aoc25",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "aoc25",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ]
-        )
-    ]
+  name: "aoc25",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "aoc25",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]
+    )
+  ]
 )
